5e4f4fbc9308a2778c8f0713232de3dd772e27c4
bitichongrourou <308498625@qq.com>
2017-06-22 17:00:28 +0800

    Initial commit

---
+++ HanYou/app/src/main/java/com/together/hanyou/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.together.hanyou;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity {
+    private Button loginButton;
+    private TextView registerText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        registerText = (TextView) findViewById(R.id.register_button);
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
